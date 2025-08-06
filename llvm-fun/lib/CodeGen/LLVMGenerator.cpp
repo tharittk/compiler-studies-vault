aa79cac1d582ba0fbce37a5ad70388101ad8508b
tharittk <tharit.tangkij@gmail.com>
2025-08-06 15:44:26 +0700

    [dirty] codegen very basic ok"

--- llvm-fun/lib/CodeGen/LLVMGenerator.cpp
+++ llvm-fun/lib/CodeGen/LLVMGenerator.cpp
@@ -52,14 +52,54 @@ Value *LLVMGenerator::allocHeap(Type *t, BasicBlock *bb, std::string name) {
   return inst;
 }
 
-// TODO Implement all visit functions and other helper functions (if any) below
-
 Value *LLVMGenerator::visit(BinExpAST *n) {
-  return NULL;
+  auto leftExp = n->getExp1AST();
+  auto rightExp = n->getExp2AST();
+
+  Value *L = leftExp->accept(*this);
+  Value *R = rightExp->accept(*this);
+  if (!L || !R)
+    reportErrorAndExit(n->getSrcLoc(), "BinExp Error: L or R Value is null");
+
+  switch (n->getOp()) {
+    case OpKind::OP_Add:
+      return this->builder.CreateAdd(L, R, "addf");
+    case OpKind::OP_Sub:
+      return this->builder.CreateSub(L, R, "subf");
+    case OpKind::OP_Mul:
+      return this->builder.CreateMul(L, R, "mulf");
+    case OpKind::OP_Equal:
+      return this->builder.CreateICmp(CmpInst::ICMP_EQ, L, R, "eqf");
+    case OpKind::OP_LT:
+      return this->builder.CreateICmp(CmpInst::ICMP_SLT, L, R, "ltf");
+    case OpKind::OP_And:
+      return this->builder.CreateAnd(L, R, "andf");
+    case OpKind::OP_Or:
+      return this->builder.CreateOr(L, R, "orf");
+    case OpKind::OP_Set:{
+      // auto Int32Ty = Type::getInt32Ty(getGlobalContext());
+      // AllocaInst *Ptr = this->builder.CreateAlloca(Int32Ty, L, "ptr");
+      return this->builder.CreateStore(R, L);
+    }
+    default:{
+      reportErrorAndExit(n->getSrcLoc(), "BinExp Error: No case matches");
+      return nullptr;
+    }
+  }
 }
 
 Value *LLVMGenerator::visit(CallExpAST *n) {
-  return NULL;
+  auto funExp = n->getFunExpAST();
+  auto argExp = n->getArgExpAST();
+
+  // simple because our language supports only one variable
+  Value* funV = funExp->accept(*this);
+  Value* argV = argExp->accept(*this);
+
+  if (!dyn_cast<Function> (funV))
+    reportErrorAndExit(n->getSrcLoc(), "CallExp Error: dyn_cast test of funV must be Function Type");
+
+  return builder.CreateCall(funV, argV, "callf");
 }
 
 Value *LLVMGenerator::visit(ConstrainExpAST *n) {
@@ -67,11 +107,18 @@ Value *LLVMGenerator::visit(ConstrainExpAST *n) {
 }
 
 Value *LLVMGenerator::visit(FunDeclAST *n) {
-  return NULL;
+
+
+
 }
 
 Value *LLVMGenerator::visit(IdExpAST *n) {
-  return NULL;
+
+  if (!ctxt.has(n->getName())) {
+    auto msg = "IdExp Error: "+ n->getName() + " is not in env";
+    reportErrorAndExit(n->getSrcLoc(), msg);
+  }
+  return ctxt.get(n->getName());
 }
 
 Value *LLVMGenerator::visit(IfExpAST *n) {
@@ -79,7 +126,7 @@ Value *LLVMGenerator::visit(IfExpAST *n) {
 }
 
 Value *LLVMGenerator::visit(IntExpAST *n) {
-  return NULL;
+  return ConstantInt::get(llvm::Type::getInt32Ty(getGlobalContext()), n->getNum(), /*isSigned=*/true);
 }
 
 Value *LLVMGenerator::visit(LetExpAST *n) {
