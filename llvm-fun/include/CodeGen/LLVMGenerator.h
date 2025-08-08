91e0b1dd3f22d43f7ebcffc0212562ac5265683e
tharittk <tharit.tangkij@gmail.com>
2025-08-08 16:11:39 +0700

    [draft] first draft of IR codegen

--- llvm-fun/include/CodeGen/LLVMGenerator.h
+++ llvm-fun/include/CodeGen/LLVMGenerator.h
@@ -3,64 +3,66 @@
 
 #include "AST/SrcLoc.h"
 #include "Util/Context.h"
+#include "AST/MyType.h"
 #include "Visitor/LLVMVisitor.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
 #include <assert.h>
 
-namespace fun {
+namespace fun
+{
 
-using namespace llvm;
+  using namespace llvm;
 
-class LLVMGenerator : public LLVMVisitor {
-public:
-  LLVMGenerator(ProgramAST *prg, std::string srcFileName)
-    : prg(prg), srcFileName(srcFileName), builder(getGlobalContext()) {}
+  class LLVMGenerator : public LLVMVisitor
+  {
+  public:
+    LLVMGenerator(ProgramAST *prg, std::string srcFileName)
+        : prg(prg), srcFileName(srcFileName), builder(getGlobalContext()) {}
 
-  Value *visit(BinExpAST *n);
-  Value *visit(CallExpAST *n);
-  Value *visit(ConstrainExpAST *n);
-  Value *visit(FunDeclAST *n);
-  Value *visit(FunTypeAST *) { return NULL; }
-  Value *visit(IdExpAST *n);
-  Value *visit(IfExpAST *n);
-  Value *visit(IntExpAST *);
-  Value *visit(IntTypeAST *) { return NULL; }
-  Value *visit(LetExpAST *n);
-  Value *visit(ProgramAST *n);
-  Value *visit(ProjExpAST *n);
-  Value *visit(RefTypeAST *) { return NULL; }
-  Value *visit(SeqExpAST *n);
-  Value *visit(TupleExpAST *n);
-  Value *visit(TupleTypeAST *) { return NULL; }
-  Value *visit(UnExpAST *n);
-  Value *visit(WhileExpAST *n);
+    Value *visit(BinExpAST *n);
+    Value *visit(CallExpAST *n);
+    Value *visit(ConstrainExpAST *n);
+    Value *visit(FunDeclAST *n);
+    Value *visit(FunTypeAST *) { return NULL; }
+    Value *visit(IdExpAST *n);
+    Value *visit(IfExpAST *n);
+    Value *visit(IntExpAST *);
+    Value *visit(IntTypeAST *) { return NULL; }
+    Value *visit(LetExpAST *n);
+    Value *visit(ProgramAST *n);
+    Value *visit(ProjExpAST *n);
+    Value *visit(RefTypeAST *) { return NULL; }
+    Value *visit(SeqExpAST *n);
+    Value *visit(TupleExpAST *n);
+    Value *visit(TupleTypeAST *) { return NULL; }
+    Value *visit(UnExpAST *n);
+    Value *visit(WhileExpAST *n);
 
-  void run(std::ostream &out);
+    void run(std::ostream &out);
 
-private:
-  // TODO You can add new helper functions or members here if you need.
-  Type* codegenType(MyType* t);
+  private:
+    Type *toLLVMType(MyType *t);
+    void reportErrorAndExit(SrcLoc srcLoc, std::string msg) const
+    {
+      std::cerr << srcFileName << ":" << srcLoc.line << ":" << srcLoc.col
+                << ": error: " << msg << "\n";
+      assert(false);
+    }
 
-  void reportErrorAndExit(SrcLoc srcLoc, std::string msg) const {
-    std::cerr << srcFileName << ":" << srcLoc.line << ":" << srcLoc.col
-              << ": error: " << msg << "\n";
-    assert(false);
-  }
+    // Allocate memory on heap based on the size of the given type.
+    // Created instructions are inserted at the end of the given basic block.
+    Value *allocHeap(Type *t, BasicBlock *bb, std::string name = "");
 
-  // Allocate memory on heap based on the size of the given type.
-  // Created instructions are inserted at the end of the given basic block.
-  Value *allocHeap(Type *t, BasicBlock *bb, std::string name="");
+    // TODO Add any helper functions you want to add
 
-  // TODO Add any helper functions you want to add
+    ProgramAST *prg;
+    std::string srcFileName;
+    Context<std::string, Value *> ctxt;
 
-  ProgramAST *prg;
-  std::string srcFileName;
-  Context<std::string, Value*> ctxt;
-
-  IRBuilder<> builder;
-  Module *module;
-};
+    IRBuilder<> builder;
+    Module *module;
+  };
 
 }
 
--- llvm-fun/lib/CodeGen/LLVMGenerator.cpp
+++ llvm-fun/lib/CodeGen/LLVMGenerator.cpp
@@ -9,15 +9,16 @@
 using namespace fun;
 using namespace llvm;
 
-void LLVMGenerator::run(std::ostream &out) {
+void LLVMGenerator::run(std::ostream &out)
+{
   InitializeNativeTarget();
   module = new Module(srcFileName, getGlobalContext());
-
   // Actually we don't need JIT feature in this assignment, but we create this
   // to get DataLayout and TargetMachine instances.
   std::string errStr;
   ExecutionEngine *engine = EngineBuilder(module).setErrorStr(&errStr).create();
-  if (!engine) {
+  if (!engine)
+  {
     std::cerr << "Could not create ExecutionEngine: " << errStr << "\n";
     exit(1);
   }
@@ -35,11 +36,12 @@ void LLVMGenerator::run(std::ostream &out) {
   delete engine;
 }
 
-Value *LLVMGenerator::allocHeap(Type *t, BasicBlock *bb, std::string name) {
+Value *LLVMGenerator::allocHeap(Type *t, BasicBlock *bb, std::string name)
+{
   const DataLayout *layout = module->getDataLayout();
   ConstantInt *allocSize =
-    ConstantInt::get(Type::getInt32Ty(getGlobalContext()),
-                     layout->getTypeAllocSize(t));
+      ConstantInt::get(Type::getInt32Ty(getGlobalContext()),
+                       layout->getTypeAllocSize(t));
 
   Instruction *inst = CallInst::CreateMalloc(
       bb,
@@ -53,7 +55,8 @@ Value *LLVMGenerator::allocHeap(Type *t, BasicBlock *bb, std::string name) {
   return inst;
 }
 
-Value *LLVMGenerator::visit(BinExpAST *n) {
+Value *LLVMGenerator::visit(BinExpAST *n)
+{
   auto leftExp = n->getExp1AST();
   auto rightExp = n->getExp2AST();
 
@@ -62,65 +65,70 @@ Value *LLVMGenerator::visit(BinExpAST *n) {
   if (!L || !R)
     reportErrorAndExit(n->getSrcLoc(), "BinExp Error: L or R Value is null");
 
-  switch (n->getOp()) {
-    case OpKind::OP_Add:
-      return this->builder.CreateAdd(L, R, "addf");
-    case OpKind::OP_Sub:
-      return this->builder.CreateSub(L, R, "subf");
-    case OpKind::OP_Mul:
-      return this->builder.CreateMul(L, R, "mulf");
-    case OpKind::OP_Equal:
-      return this->builder.CreateICmp(CmpInst::ICMP_EQ, L, R, "eqf");
-    case OpKind::OP_LT:
-      return this->builder.CreateICmp(CmpInst::ICMP_SLT, L, R, "ltf");
-    case OpKind::OP_And:
-      return this->builder.CreateAnd(L, R, "andf");
-    case OpKind::OP_Or:
-      return this->builder.CreateOr(L, R, "orf");
-    case OpKind::OP_Set:{
-      // auto Int32Ty = Type::getInt32Ty(getGlobalContext());
-      // AllocaInst *Ptr = this->builder.CreateAlloca(Int32Ty, L, "ptr");
-      return this->builder.CreateStore(R, L);
-    }
-    default:{
-      reportErrorAndExit(n->getSrcLoc(), "BinExp Error: No case matches");
-      return nullptr;
-    }
+  switch (n->getOp())
+  {
+  case OpKind::OP_Add:
+    return this->builder.CreateAdd(L, R, "addf");
+  case OpKind::OP_Sub:
+    return this->builder.CreateSub(L, R, "subf");
+  case OpKind::OP_Mul:
+    return this->builder.CreateMul(L, R, "mulf");
+  case OpKind::OP_Equal:
+    return this->builder.CreateICmp(CmpInst::ICMP_EQ, L, R, "eqf");
+  case OpKind::OP_LT:
+    return this->builder.CreateICmp(CmpInst::ICMP_SLT, L, R, "ltf");
+  case OpKind::OP_And:
+    return this->builder.CreateAnd(L, R, "andf");
+  case OpKind::OP_Or:
+    return this->builder.CreateOr(L, R, "orf");
+  case OpKind::OP_Set:
+    return this->builder.CreateStore(R, L);
+  default:
+  {
+    reportErrorAndExit(n->getSrcLoc(), "BinExp Error: No case matches");
+    return nullptr;
+  }
   }
 }
 
-Value *LLVMGenerator::visit(CallExpAST *n) {
+Value *LLVMGenerator::visit(CallExpAST *n)
+{
   auto funExp = n->getFunExpAST();
   auto argExp = n->getArgExpAST();
 
   // simple because our language supports only one variable
-  Value* funV = funExp->accept(*this);
-  if (!dyn_cast<Function> (funV))
+  Value *funV = funExp->accept(*this);
+  if (!dyn_cast<Function>(funV))
     reportErrorAndExit(n->getSrcLoc(), "CallExp Error: dyn_cast test of funV must be Function Type");
   Function *CalleeF = module->getFunction(funV->getName());
 
-  Value* argV = argExp->accept(*this);
+  Value *argV = argExp->accept(*this);
   return builder.CreateCall(CalleeF, argV, "callf");
 }
 
-Value *LLVMGenerator::visit(ConstrainExpAST *n) {
-
+Value *LLVMGenerator::visit(ConstrainExpAST *n)
+{
+  // constrain is for type checking. It doesn't make
+  // any difference in IR generation
+  return n->getExpAST()->accept(*this);
 }
 
-Value *LLVMGenerator::visit(FunDeclAST *n) {
+Value *LLVMGenerator::visit(FunDeclAST *n)
+{
   // register function to environment
   auto retTy = MyType::getType(n->getRetTypeAST());
   auto paramTy = MyType::getType(n->getParamTypeAST());
   // case swtich, int, ref, fun ,tuple, unit
-  FunctionType *FT = FunctionType::get(/*return ty*/codegenType(retTy),
-                                  /*arg ty*/codegenType(paramTy), /*vararg*/ false);
+  FunctionType *FT = FunctionType::get(/*return ty*/ toLLVMType(retTy),
+                                       /*arg ty*/ toLLVMType(paramTy), /*vararg*/ false);
 
   // register to the module for symbol look up
   Function *F = Function::Create(FT, Function::ExternalLinkage, n->getName(), module);
 
   // llvm semantic, if the symbol already exists, F will be automatically renamed
   // and thus not equal to intended name
-  if (F->getName() != n->getName()){
+  if (F->getName() != n->getName())
+  {
     F->eraseFromParent();
     F = module->getFunction(n->getName());
   }
@@ -130,28 +138,32 @@ Value *LLVMGenerator::visit(FunDeclAST *n) {
   AI->setName(n->getParamName());
 
   // no need for this since LLVM uses module "context" for functions
-  // ctxt.bind(n->getName(), F);
-  // ctxt.bind(n->getParamName(), );
   BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", F);
   builder.SetInsertPoint(BB);
-  if(Value* retVal = n->getBodyExpAST()->accept(*this)){
+  if (Value *retVal = n->getBodyExpAST()->accept(*this))
+  {
     builder.CreateRet(retVal);
-    verifyFunction(*F); 
+    verifyFunction(*F);
     return F;
   }
+  return NULL;
 }
 
-Value *LLVMGenerator::visit(IdExpAST *n) {
-  if (!ctxt.has(n->getName())) {
-    auto msg = "IdExp Error: "+ n->getName() + " is not in env";
+Value *LLVMGenerator::visit(IdExpAST *n)
+{
+  if (!ctxt.has(n->getName()))
+  {
+    auto msg = "IdExp Error: " + n->getName() + " is not in env";
     reportErrorAndExit(n->getSrcLoc(), msg);
   }
   return ctxt.get(n->getName());
 }
 
-Value *LLVMGenerator::visit(IfExpAST *n) {
-  Value* condV = n->getCondExpAST()->accept(*this); //codegen
-  if (condV == 0) return 0;
+Value *LLVMGenerator::visit(IfExpAST *n)
+{
+  Value *condV = n->getCondExpAST()->accept(*this); // codegen
+  if (condV == 0)
+    return 0;
   // convert condition to a bool
   condV = builder.CreateICmpNE(condV, ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0), "ifcond");
   Function *F = builder.GetInsertBlock()->getParent();
@@ -166,8 +178,9 @@ Value *LLVMGenerator::visit(IfExpAST *n) {
 
   // set current insertion point to the end of ThenBB but ThenBB is empty so we are ok
   builder.SetInsertPoint(ThenBB);
-  Value* thenV = n->getThenExpAST()->accept(*this); //codegen
-  if (thenV == 0) return 0;
+  Value *thenV = n->getThenExpAST()->accept(*this); // codegen
+  if (thenV == 0)
+    return 0;
   // unconditional branch then->merge. LLVM requires that every BB has a terminator (ret or br)
   builder.CreateBr(MergeBB);
   // codegen of 'then' may change the current block i.e., it recurses to somewhere else
@@ -177,7 +190,7 @@ Value *LLVMGenerator::visit(IfExpAST *n) {
   // emit else block
   F->getBasicBlockList().push_back(ElseBB); // then is added through the Create()
   builder.SetInsertPoint(ElseBB);
-  Value* elseV;
+  Value *elseV;
   if (n->getElseExpAST() != nullptr)
     elseV = n->getElseExpAST()->accept(*this);
   else
@@ -196,70 +209,190 @@ Value *LLVMGenerator::visit(IfExpAST *n) {
   return PN;
 }
 
-Value *LLVMGenerator::visit(IntExpAST *n) {
+Value *LLVMGenerator::visit(IntExpAST *n)
+{
   return ConstantInt::get(Type::getInt32Ty(getGlobalContext()), n->getNum(), /*isSigned=*/true);
 }
 
-Value *LLVMGenerator::visit(LetExpAST *n) {
+Value *LLVMGenerator::visit(LetExpAST *n)
+{
+  auto varName = n->getVarName();
+  Value *varExpV = n->getVarExpAST()->accept(*this);
+  auto ck = ctxt.checkPoint();
+  ctxt.bind(varName, varExpV);
+  // eval bodyVal from codegen
+  Value *bodyV = n->getBodyExpAST()->accept(*this);
+  // restore old variable
+  ctxt.restoreCheckPoint(ck);
+  return bodyV;
+}
+
+Value *LLVMGenerator::visit(ProgramAST *n)
+{
+  const std::map<std::string, FunDeclAST *> &funDecls = n->getFunDeclASTs();
+  for (auto it = funDecls.begin(); it != funDecls.end(); ++it)
+  {
+    it->second->accept(*this);
+  }
   return NULL;
 }
 
-Value *LLVMGenerator::visit(ProgramAST *n) {
-  return NULL;
+Value *LLVMGenerator::visit(ProjExpAST *n)
+{
+  Value *targetTupleAlloca = n->getTargetTupleExpAST()->accept(*this);
+  // if (!tupleV) // must be tuple/vector, depending on how you represent it
+  //   reportErrorAndExit(n->getSrcLoc(), "ProjExp Error: target Value must be a vector");
+  Value *ptr = builder.CreateStructGEP(targetTupleAlloca, n->getIndex(), "load.ptr");
+  Value *loadInst = builder.CreateLoad(ptr, "projLoad");
+  return loadInst;
 }
 
-Value *LLVMGenerator::visit(ProjExpAST *n) {
-  return NULL;
+Value *LLVMGenerator::visit(SeqExpAST *n)
+{
+  n->getExp1AST()->accept(*this);
+  Value *v = n->getExp2AST()->accept(*this);
+  return v;
 }
 
-Value *LLVMGenerator::visit(SeqExpAST *n) {
-  return NULL;
+Value *LLVMGenerator::visit(TupleExpAST *n)
+{
+  std::vector<Value *> tupleV;
+  std::vector<Type *> tupleTy;
+
+  for (const auto exp : n->getExpASTs())
+  {
+    Value *expV = exp->accept(*this);
+    tupleV.push_back(expV);
+    tupleTy.push_back(expV->getType());
+  }
+
+  StructType *elemTy = StructType::get(getGlobalContext(), tupleTy);
+  Value *structAlloca = builder.CreateAlloca(elemTy, nullptr, "tuple.alloca");
+
+  for (size_t i = 0; i < tupleV.size(); ++i)
+  {
+    Value *fieldPtr = builder.CreateStructGEP(structAlloca, i, "tuple.ptr");
+    builder.CreateStore(tupleV[i], fieldPtr);
+  }
+  return structAlloca;
 }
 
-Value *LLVMGenerator::visit(TupleExpAST *n) {
-  return NULL;
+Value *LLVMGenerator::visit(UnExpAST *n)
+{
+
+  switch (n->getOp())
+  {
+  case OpKind::OP_UMinus:
+  {
+    // apply minus to exp
+    Value *v = n->getExp1AST()->accept(*this);
+    ConstantInt *constInt = dyn_cast<ConstantInt>(v);
+    if (!constInt)
+      reportErrorAndExit(n->getSrcLoc(), "UMinus must be applied to Int");
+    return ConstantInt::get(v->getType(), -constInt->getValue());
+  }
+  case OpKind::OP_Not:
+  {
+    // compare with zero,
+    Function *F = builder.GetInsertBlock()->getParent();
+    BasicBlock *condBB = BasicBlock::Create(getGlobalContext(), "notCond", F);
+    BasicBlock *trueBB = BasicBlock::Create(getGlobalContext(), "trueBr");
+    BasicBlock *falseBB = BasicBlock::Create(getGlobalContext(), "falseBr");
+    BasicBlock *MergeBB = BasicBlock::Create(getGlobalContext(), "mergeNot");
+
+    builder.CreateBr(condBB);
+    builder.SetInsertPoint(condBB);
+
+    Value *v = n->getExp1AST()->accept(*this);
+    Value *condV = builder.CreateICmpNE(v, ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0), "notCond");
+    builder.CreateCondBr(condV, /*true*/ trueBB, /*false*/ falseBB);
+
+    F->getBasicBlockList().push_back(trueBB);
+    builder.SetInsertPoint(trueBB);
+    Value *zeroV = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0, /*isSigned=*/true);
+
+    builder.CreateBr(MergeBB);
+
+    F->getBasicBlockList().push_back(falseBB);
+    builder.SetInsertPoint(falseBB);
+    Value *oneV = ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 1, /*isSigned=*/true);
+    builder.CreateBr(MergeBB);
+
+    F->getBasicBlockList().push_back(MergeBB);
+    builder.SetInsertPoint(MergeBB);
+
+    PHINode *PN = builder.CreatePHI(Type::getInt32Ty(getGlobalContext()), 2, "nottmp");
+    PN->addIncoming(zeroV, trueBB);
+    PN->addIncoming(oneV, falseBB);
+    return PN;
+  }
+  case OpKind::OP_Ref:
+  {
+    // create a ptr from it
+    Value *expV = n->getExp1AST()->accept(*this);
+    Value *allocInst = builder.CreateAlloca(expV->getType(), expV, "allocaRef");
+    Value *ptr = builder.CreateGEP(allocInst, 0, "refPtr");
+    return ptr;
+  }
+
+  case OpKind::OP_Get:
+  {
+    Value *expV = n->getExp1AST()->accept(*this);
+    // you may do dyn_cast here
+    return builder.CreateLoad(expV, "loadGet");
+  }
+  default:
+    reportErrorAndExit(n->getSrcLoc(), "UnExp Error: case does not match");
+    return NULL;
+  }
 }
 
-Value *LLVMGenerator::visit(UnExpAST *n) {
-  return NULL;
-}
-
-Value *LLVMGenerator::visit(WhileExpAST *n) {
+Value *LLVMGenerator::visit(WhileExpAST *n)
+{
 
   Function *F = builder.GetInsertBlock()->getParent();
-  BasicBlock* CondBB = BasicBlock::Create(getGlobalContext(), "whileCond", F);
-  BasicBlock* LoopBB = BasicBlock::Create(getGlobalContext(), "loop");
-  BasicBlock* AfterLoopBB = BasicBlock::Create(getGlobalContext(), "afterLoop");
+  BasicBlock *CondBB = BasicBlock::Create(getGlobalContext(), "whileCond", F);
+  BasicBlock *LoopBB = BasicBlock::Create(getGlobalContext(), "loop");
+  BasicBlock *AfterLoopBB = BasicBlock::Create(getGlobalContext(), "afterLoop");
 
   // connect the (whatever it is) current block to the CondBB unconditionally
   builder.CreateBr(CondBB);
 
   builder.SetInsertPoint(CondBB);
-  Value* condV = n->getCondExpAST()->accept(*this);
-  if (condV == 0) return 0;
+  Value *condV = n->getCondExpAST()->accept(*this);
+  if (condV == 0)
+    return 0;
   condV = builder.CreateICmpNE(condV, ConstantInt::get(Type::getInt32Ty(getGlobalContext()), 0), "whileCond");
-  builder.CreateCondBr(condV, /*true*/LoopBB, /*false*/AfterLoopBB); // emit right after cmp
+  builder.CreateCondBr(condV, /*true*/ LoopBB, /*false*/ AfterLoopBB); // emit right after cmp
 
   builder.SetInsertPoint(LoopBB);
-  Value* bodyV = n->getBodyExpAST()->accept(*this); //codegen
-  if (bodyV == 0) return 0;
-  builder.CreateBr(CondBB); //backedge to check the condition
+  Value *bodyV = n->getBodyExpAST()->accept(*this); // codegen
+  if (bodyV == 0)
+    return 0;
+  builder.CreateBr(CondBB); // backedge to check the condition
   // while should return unit type
   return Constant::getNullValue(Type::getInt32Ty(getGlobalContext()));
 }
 
-Type* codegenType(MyType *t){
-  if (auto intTy = MyType::toIntType(t)){
+Type *LLVMGenerator::toLLVMType(MyType *t)
+{
+  if (t->isIntType())
+  {
     return Type::getInt32Ty(getGlobalContext());
-  } else if (auto refTy = MyType::toRefType(t)){
-    // ref type
-  } else if (auto funTy = MyType::toFunType(t)){
-    // fun type
-  } else if (auto tupleTy = MyType::toTupleType(t)){
-    // tutple type
-    // append to vector
   }
-
-  llvm_unreachable("Type not specified in the language");
-  return NULL;
+  else if (t->isRefType())
+  {
+    return Type::getInt32PtrTy(getGlobalContext(), true);
+  }
+  else if (t->isTupleType())
+  {
+    // TODO: what should be here ? The is no generalize struct type
+    return Type::getVoidTy(getGlobalContext());
+  }
+  else
+  {
+    /* likely function type */
+    std::cerr << "You use type from MyType -> LLVM Type the wrong way !";
+    return Type::getVoidTy(getGlobalContext());
+  }
 }
